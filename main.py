23fa2f5f117db313f00818941b619859e8d0dd39
Lauren Phillips <gail2104lp@gmail.com>
2025-04-02 12:04:05 -0500

    Implemented functionality for login and sign up, and directed to appropriate screens

--- main.py
+++
@@ -1,7 +0,0 @@
-#entry point for app
-from website import create_app
-
-app = create_app()
-
-if __name__ == "__main__": #only runs web server if this file is run directly
-    app.run(debug = True) #runs server and debug = True means any change to the code will update the server (will be turned off once up)
